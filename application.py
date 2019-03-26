2e33dc4bf1ae91ee53ac09f4960848bb683a6caf
dannguyen99 <dannguyen0801@gmail.com>
2019-03-26 22:31:57 +0700

    added register.html and modify some color elements

--- application.py
+++ application.py
@@ -1,13 +1,17 @@
 import os
 
 from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+#from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
-socketio = SocketIO(app)
+#socketio = SocketIO(app)
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
