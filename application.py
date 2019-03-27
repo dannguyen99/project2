d9f127c045611bbfbd4a55bb688f14a9f2a95619
dannguyen99 <dannguyen0801@gmail.com>
2019-03-27 13:40:10 +0700

    added models.py

--- application.py
+++ application.py
@@ -2,14 +2,20 @@ import os
 
 from flask import Flask, render_template
 #from flask_socketio import SocketIO, emit
+from models import *
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
 #socketio = SocketIO(app)
 
 
-@app.route("/")
+@app.route("/", methods = ["GET", "POST"])
 def index():
+    if method == "POST":
+
     return render_template("index.html")
 
 @app.route("/register")
