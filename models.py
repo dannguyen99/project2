d26664c2f5ee286fc0855053a881d77169b1423c
dannguyen99 <dannguyen0801@gmail.com>
2019-04-03 23:49:28 +0700

    modified channels.html and models.html

--- models.py
+++ models.py
@@ -10,4 +10,9 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, nullable = False)
     password = db.Column(db.String, nullable = False)
-    
+
+class Channels(db.Model):
+    __tablename__ = "channels"
+    channel_name = db.Column(db.String, nullable = False)
+    no_people = db.Column(db.Integer, default = 0)
+    messages = db.Column(db.Integer, default = 0)
