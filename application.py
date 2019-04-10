c8f458cece4ea4378c7df3b0cb1264461006f748
= <dannguyen99@gmail.com>
2019-04-10 14:03:11 +0700

    add some minnor changes

--- application.py
+++ application.py
@@ -10,6 +10,7 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['DEBUG'] = False
 db.init_app(app)
 socketio = SocketIO(app)
 
