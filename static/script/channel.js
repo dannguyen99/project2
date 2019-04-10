7254a97df2eec6bba29a73276897a2494ad632cb
= <dannguyen99@gmail.com>
2019-04-10 15:44:42 +0700

    so tired cant doit anymore

--- static/script/channel.js
+++ static/script/channel.js
@@ -15,8 +15,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
       // When a new vote is announced, add to the unordered list
       socket.on('announce message', data => {
-          const h2 = document.createElement('h2');
-          h2.innerHTML = data.mess;
-          document.querySelector('.container').appendChild(h2);
+        alert("success");
+        const h2 = document.createElement('h2');
+        h2.innerHTML = data.mess;
+        document.querySelector('.container').appendChild(h2);
       });
   });
